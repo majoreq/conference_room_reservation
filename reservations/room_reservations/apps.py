6a1cececc93722ca6cf65ec4aa35d4d7365ac20f
Krzysztof Mazurkiewicz <k.mazurkiewicz.91@gmail.com>
2018-06-09 12:46:55 +0200

    created models

---
+++ reservations/room_reservations/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RoomReservationsConfig(AppConfig):
+    name = 'room_reservations'
---
+++ reservations/room_reservations/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Room(models.Model):
+    name = models.CharField(max_length=64, verbose_name="name")
+    capacity = models.IntegerField(verbose_name="capacity")
+    projector = models.BooleanField(verbose_name="projector")
+
+class Reservation(models.Model):
+    date = models.DateField(verbose_name="date")
+    room_id = models.ForeignKey(Room, verbose_name="room_id", on_delete=models.SET_NULL, null=True)
+    comment = models.TextField(verbose_name="comment")
